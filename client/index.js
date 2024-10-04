65c909b63a37d29aa00dd64041ffbee89ca88a4a
Maria <marfayaga@gmail.com>
2024-10-04 20:21:55 +0300

    implement ability to add tasks

--- client/index.js
+++ client/index.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+'use strict';
+
+const todoInputEl = document.querySelector('.todo-input');
+const addBtnEl = document.querySelector('.add-todo-btn');
+
+const todoListEl = document.querySelector('.todo-list');
+
+function createTodoItem(text, parent) {
+  const itemEl = document.createElement('li');
+  itemEl.className = 'todo-list-item';
+  itemEl.innerHTML = `<span class="todo-list-item-text">${text}</span>
+    <button class="edit-todo-btn">Редагувати</button>
+    <button class="delete-todo-btn">Видалити</button>`;
+  
+  parent.append(itemEl);
+}
+
+addBtnEl.addEventListener('click', event => {
+  const newTaskText = todoInputEl.value.trim();
+  if (!newTaskText) {
+    return;
+  }
+
+  createTodoItem(newTaskText, todoListEl);
+})
