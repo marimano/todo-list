b4eeea190a720e22a8f1a3ef669be80cf831f7da
Maria <marfayaga@gmail.com>
2024-10-04 20:54:29 +0300

    implement ability to edit a task

--- client/index.js
+++ client/index.js
@@ -27,12 +27,44 @@ addBtnEl.addEventListener('click', event => {
 
 todoListEl.addEventListener('click', event => {
   event.stopPropagation();
+  const { target } = event;
+  const todoItemEl = target.closest(".todo-list-item");
+  switch (target.className) {
+    case 'delete-todo-btn': {
+      const taskText = todoItemEl.querySelector('.todo-list-item-text').textContent;
+      if (confirm(`Ти добре подумав і хочеш видалити ${taskText} ?`)) {
+        todoItemEl.remove();
+      }
+      break;
+    }
 
-  if (event.target.className === 'delete-todo-btn') {
-    const todoItemEl = event.target.closest(".todo-list-item");
-    const taskText = todoItemEl.querySelector('.todo-list-item-text').textContent;
-    if (confirm(`Ти добре подумав і хочеш видалити ${taskText} ?`)) {
-      todoItemEl.remove();
+    case 'edit-todo-btn': {
+      const taskText = todoItemEl.querySelector('.todo-list-item-text').textContent;
+      todoItemEl.innerHTML = `<input class="todo-list-item-text" value="${taskText}" default-value="${taskText}"/>
+        <button class="save-todo-btn">Зберегти</button>
+        <button class="cancel-todo-btn">Закрити</button>`;
+      break;
+    }
+
+    case 'save-todo-btn': {
+      const updatedTaskText = target.previousElementSibling.value.trim();
+      if (!updatedTaskText) {
+        return;
+      }
+
+      todoItemEl.innerHTML = `<span class="todo-list-item-text">${updatedTaskText}</span>
+        <button class="edit-todo-btn">Редагувати</button>
+        <button class="delete-todo-btn">Видалити</button>`;
+      break;
+    }
+
+    case 'cancel-todo-btn': {
+      const oldTaskText = target.previousElementSibling.previousElementSibling.defaultValue;
+
+      todoItemEl.innerHTML = `<span class="todo-list-item-text">${oldTaskText}</span>
+        <button class="edit-todo-btn">Редагувати</button>
+        <button class="delete-todo-btn">Видалити</button>`;
+      break;
     }
   }
 });
